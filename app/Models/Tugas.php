e6a7ca4d91eb6e4fa211a2478cafb81214cb802a
tzelginianc <tzelginia78@gmail.com>
2022-10-31 18:19:58 +0700

    Model Tugas

---
+++ app/Models/Tugas.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tugas extends Model
+{
+    protected $table = 'pertemuans';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'subject',
+        'pdf',
+        'mapel_id',
+        'pertemuan_id'
+    ];
+
+    public function mapel()
+    {
+        return $this->belongsTo(Mapel::class);
+    }
+    public function pertemuan()
+    {
+        return $this->belongsTo(Pertemuan::class);
+    }
+}
